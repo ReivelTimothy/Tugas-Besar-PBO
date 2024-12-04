8cf935102d5b328aa91cc7f75d0183af0fc177fa
AlfonS5S5S5 <r.jonathanalfons@gmail.com>
2024-12-05 00:06:49 +0700

    commit

--- Models/classes/EventConcert.java
+++ Models/classes/EventConcert.java
@@ -2,10 +2,11 @@ package Models.classes;
 
 import java.sql.Date;
 
-public abstract class EventConcert extends Event{
-    String jenisKonser, singer;
+public class EventConcert extends Event{
+    private String jenisKonser, singer;
+
     public EventConcert(String title, String location, String snk, String vendor, String dateTime, String information,
-            double price, double discount, int rating, String jenisKonser, String singer, Date tanggal) {
+            double price, double discount, int rating, Date tanggal, String jenisKonser, String singer) {
         super(title, location, snk, vendor, dateTime, information, price, discount, rating, tanggal);
         this.jenisKonser = jenisKonser;
         this.singer = singer;
@@ -14,18 +15,13 @@ public abstract class EventConcert extends Event{
     public String getJenisKonser() {
         return jenisKonser;
     }
-
     public void setJenisKonser(String jenisKonser) {
         this.jenisKonser = jenisKonser;
     }
-
     public String getSinger() {
         return singer;
     }
-
     public void setSinger(String singer) {
         this.singer = singer;
     }
-
-    
 }
