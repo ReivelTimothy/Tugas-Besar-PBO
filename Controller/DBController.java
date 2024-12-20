3f8088673aa0cbde11b4f02f74cb3c4234171cff
ReivelTimothy <timothyreivel1@gmail.com>
2024-12-20 11:59:45 +0700

    update Controler and add connector

--- Controller/DBController.java
+++ Controller/DBController.java
@@ -1,8 +1,17 @@
 package Controller;
 
+import java.awt.Event;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+import Models.classes.EventConcert;
+import Models.enumaration.EventCat;
+import Models.enumaration.statusBlocked;
 
 public class DBController {
 
@@ -62,24 +71,36 @@ public class DBController {
         }
     }
 
-    // public static boolean insertNewUser(User user) {
-    //     String query = "INSERT INTO  ";
-    //     try {
-    //         conn.connect();
-    //         PreparedStatement statement = conn.con.prepareStatement(query);
-           
+    public static boolean addConcert(EventConcert event){
+        String query = 
+        "INSERT INTO events (event_id, judul, harga, eventStart, eventEnd, description, kategori, capacity, date, seller_id) " +
+        " VALUES (?,?,?,?,?,?,?,?,?,?)";
+        try {
+            conn.connect();
+            java.sql.Date sqlDate = new java.sql.Date((event.getTanggal()).getTime());
+            PreparedStatement stmt = conn.con.prepareStatement(query);
+            stmt.setInt(1, 0);
+            stmt.setString(2, event.getTitle());
+            stmt.setDouble(3, event.getPrice());
+            stmt.setString(4, event.getTimeStart());
+            stmt.setString(5, event.getTimeEnd());
+            stmt.setString(6, event.getDesc());
+            stmt.setString(7, EventCat.getEnum(EventCat.MUSIC));
+            stmt.setInt(8, event.getCapacity());
+            stmt.setDate(9, sqlDate);
+            stmt.setInt(10, 0);
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "EVENT ANDA TELAH TERDAFTAR ");
 
-    //         statement.executeUpdate();
-    //         return true;
-    //     }
-    //     catch (SQLException e) {
-    //         e.printStackTrace();
-    //         return false;
-    //     }
-    //     finally {
-    //         conn.disconnect();
-    //     }
-    // }
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        
+    }
 
     // public static boolean deleteData() {
     //     String query = "DELETE FROM  WHERE ";
--- Controller/DatabaseHandler.java
+++ Controller/DatabaseHandler.java
@@ -3,16 +3,13 @@ package Controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-// import java.sql.SQLException;
-// import java.util.TimeZone;
 import javax.swing.JOptionPane;
 
 public class DatabaseHandler {
 
     public Connection con;
     private String driver = "com.mysql.cj.jdbc.Driver";
-    private String url = "jdbc:mysql://localhost/tickethbo";
-    // private String url = "jdbc:mysql://localhost/db_test?serverTimezone=" + TimeZone.getDefault().getID();
+    private String url = "jdbc:mysql://localhost/tickethbo?serverTimezone=UTC&useSSL=false";
     private String username = "root";
     private String password = "";
 
