44cbb9d648b8af514eded248b2af9aa21c178e00
Steven Pangestu <stevenpangestu02@gmail.com>
2025-01-01 14:42:13 +0700

    commit loginCheck

--- Controller/LoginCheck.java
+++ Controller/LoginCheck.java
@@ -10,40 +10,113 @@ public class LoginCheck {
 
     static DatabaseHandler conn = new DatabaseHandler();
 
-    public LoginCheck(String usn, String password) {
-        cekUser(usn, password);
+    public LoginCheck(String email, String password) {
+        cekCustomer(email, password);
     }
 
-    public static void cekUser(String username, String password) {
-        String usernameLogin = "";
+    public static void cekCustomer(String email, String password) {
+        String emailLogin = "";
         String pass = "";
 
         try {
             conn.connect();
-            // String query = "SELECT * FROM users where email='" + email + "' && password='" + password + "'";
+            String query = "SELECT * FROM customer where email='" + email + "' AND password='" + password + "'";
 
             Statement stmt = conn.con.createStatement();
             ResultSet rs = stmt.executeQuery(query);
 
             while (rs.next()) {
-                loginEmail = rs.getString("email");
+                emailLogin = rs.getString("email");
                 pass = rs.getString("password");
             }
 
-            if (loginEmail.equals(email) && pass.equals(password)) {
-                JOptionPane.showMessageDialog(null, "Welcome!");
-            } else if (loginEmail.equals(email) && (!pass.equals(password))) {
-                JOptionPane.showMessageDialog(null, "Cek password nya lagi.");
-            } else if ((!loginEmail.equals(email)) && pass.equals(password)) {
-                JOptionPane.showMessageDialog(null, "Cek email nya lagi.");
+            if (emailLogin.equals(email) && pass.equals(password)) {
+                JOptionPane.showMessageDialog(null, "Welcome Customer !");
+            } else if (emailLogin.equals(email) && (!pass.equals(password))) {
+                JOptionPane.showMessageDialog(null, "Password salah.");
+            } else if ((!emailLogin.equals(email)) && pass.equals(password)) {
+                JOptionPane.showMessageDialog(null, "email salah.");
             } else {
-                JOptionPane.showMessageDialog(null, "Login gagal. Cek lagi email atau password nya.", "Error", JOptionPane.ERROR_MESSAGE);
+                cekSeller(email, password);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Database error: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Database error: " + e.getMessage(), "Error",
+                    JOptionPane.ERROR_MESSAGE);
         } finally {
             conn.disconnect();
         }
     }
+
+    public static void cekSeller(String email, String password) {
+        String emailLogin = "";
+        String pass = "";
+
+        try {
+            conn.connect();
+            String query = "SELECT * FROM seller where email='" + email + "' AND password='" + password + "'";
+
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                emailLogin = rs.getString("email");
+                pass = rs.getString("password");
+            }
+
+            if (emailLogin.equals(email) && pass.equals(password)) {
+                JOptionPane.showMessageDialog(null, "Welcome Seller!");
+            } else if (emailLogin.equals(email) && (!pass.equals(password))) {
+                JOptionPane.showMessageDialog(null, "Cek password nya lagi.");
+            } else if ((!emailLogin.equals(email)) && pass.equals(password)) {
+                JOptionPane.showMessageDialog(null, "Cek email nya lagi.");
+            } else {
+                cekAdmin(email, password);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Database error: " + e.getMessage(), "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } finally {
+            conn.disconnect();
+        }
+    }
+
+    public static void cekAdmin(String email, String password) {
+        String emailLogin = "";
+        String pass = "";
+
+        try {
+            conn.connect();
+            String query = "SELECT * FROM seller where email='" + email + "' AND password='" + password + "'";
+
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                emailLogin = rs.getString("email");
+                pass = rs.getString("password");
+            }
+
+            if (emailLogin.equals(email) && pass.equals(password)) {
+                JOptionPane.showMessageDialog(null, "Welcome Admin!");
+            } else if (emailLogin.equals(email) && (!pass.equals(password))) {
+                JOptionPane.showMessageDialog(null, "Cek password nya lagi.");
+            } else if ((!emailLogin.equals(email)) && pass.equals(password)) {
+                JOptionPane.showMessageDialog(null, "Cek email nya lagi.");
+            } else {
+
+                JOptionPane.showMessageDialog(null, "Login gagal. Cek lagi email atau password nya.", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Database error: " + e.getMessage(), "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } finally {
+            conn.disconnect();
+        }
+    }
+
 }
