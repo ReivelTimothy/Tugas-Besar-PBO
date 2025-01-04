66c2ed77a53ddad403f629d989a8f637da5cba21
StevenPangestu <stevenpangestu02@gmail.com>
2025-01-04 15:58:59 +0700

    commit DeleteEvent

---
+++ Controller/Admin/DeleteEvent.java
@@ -0,0 +1,40 @@
+package Controller.Admin;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Controller.DatabaseHandler;
+
+public class DeleteEvent {
+    static DatabaseHandler conn = new DatabaseHandler();
+
+    public DeleteEvent(int eventID) {
+        Delete(eventID);
+    }
+
+    public void Delete(int eventId) {
+        String queryDeleteEvent = "DELETE FROM events WHERE event_id = ? ";
+        String queryDeleteTicket = "DELETE FROM ticket WHERE event_id = ? ";
+        
+        System.out.println(queryDeleteEvent);
+        try {
+            conn.connect();
+            PreparedStatement stmtDeleteEvent = conn.con.prepareStatement(queryDeleteEvent);
+            PreparedStatement stmtDeleteTicket = conn.con.prepareStatement(queryDeleteTicket);
+
+            stmtDeleteEvent.setInt(1, eventId);
+            stmtDeleteTicket.setInt(1, eventId);
+
+            stmtDeleteTicket.executeUpdate();
+            stmtDeleteEvent.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            conn.disconnect();
+        }
+    }
+}
--- Main/Main.java
+++ Main/Main.java
@@ -1,10 +1,11 @@
 package Main;
 
 import View.*;
+import Controller.Admin.*;
 
 public class Main {
     public static void main(String[] args) {
-        Login login = new Login();
-
+        // Login login = new Login();
+        new DeleteEvent(4);
     }
 }
