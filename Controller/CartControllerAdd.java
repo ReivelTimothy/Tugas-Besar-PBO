a5b2b26d5ae0ab3bbcb84116f7cad87a6379593f
AlfonS5S5S5 <r.jonathanalfons@gmail.com>
2025-01-04 14:58:25 +0700

    update cart & controller

--- Controller/CartControllerAdd.java
+++ Controller/CartControllerAdd.java
@@ -6,14 +6,14 @@ import java.sql.SQLException;
 public class CartControllerAdd {
     static DatabaseHandler conn = new DatabaseHandler();
 
-    public static boolean addToCart(String eventId, String ticketId, String customerId) {
+    public static boolean addToCart(int eventId, String ticketId, String customerId) {
         String query = "INSERT INTO cart (event_id, ticket_id, cust_id) VALUES (?, ?, ?)";
 
         try {
             conn.connect();
 
             PreparedStatement statement = conn.con.prepareStatement(query);
-            statement.setString(1, eventId);
+            statement.setInt(1, eventId);
             statement.setString(2, ticketId);
             statement.setString(3, customerId);
 
