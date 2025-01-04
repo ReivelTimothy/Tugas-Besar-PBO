2cc7fa8e670194a4956315e6e542b93f936ec5c6
StevenPangestu <stevenpangestu02@gmail.com>
2025-01-04 18:50:51 +0700

    commit edit event

--- Controller/Admin/EditEventPrice.java
+++ Controller/Admin/EditEventPrice.java
@@ -1,5 +1,34 @@
 package Controller.Admin;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import Controller.DatabaseHandler;
+
 public class EditEventPrice {
-    
+    static DatabaseHandler conn = new DatabaseHandler();
+
+    public EditEventPrice(int eventID, double price) {
+        EditPrice(eventID, price);
+    }
+
+    public void EditPrice(int id, double price) {
+        String queryEditPrice = "UPDATE events SET harga = ? WHERE event_id = ? ";
+
+        try {
+            conn.connect();
+            PreparedStatement stmtEditPrice = conn.con.prepareStatement(queryEditPrice);
+           
+            stmtEditPrice.setDouble(1, price);
+            stmtEditPrice.setDouble(2, id);
+            
+            stmtEditPrice.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            conn.disconnect();
+        }
+    }
 }
