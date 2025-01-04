443aee26711778a5084cd546f3fba43452662a97
AlfonS5S5S5 <r.jonathanalfons@gmail.com>
2025-01-04 15:42:22 +0700

    change name file

--- Controller/ControllerUser.java
+++ Controller/ControllerUser.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Controller {
+public class ControllerUser {
     static DatabaseHandler conn = new DatabaseHandler();
 
     public double getBalance() {
