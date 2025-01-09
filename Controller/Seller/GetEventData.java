a1efa21ff89dbf8d1f3b8912e5fa1919170dcff6
ReivelTimothy <timothyreivel1@gmail.com>
2025-01-10 03:30:10 +0700

    add getEventData

--- Controller/Seller/GetEventData.java
+++ Controller/Seller/GetEventData.java
@@ -7,36 +7,66 @@ import java.sql.SQLException;
 import Models.Classess.Event;
 import Controller.DatabaseHandler;
 import Models.Classess.EventConcert;
+import Models.Classess.EventEducation;
+import Models.Classess.EventSport;
+import Models.Enumeration.EventCat;
 
 public class GetEventData {
     public static void main(String[] args) {
-        System.out.println(getData("2312"));
+        Event event = getData("assets\\2.png");
+        System.out.println(event.getTitle());
     }
 
     static DatabaseHandler conn = new DatabaseHandler();
 
-    public static Event getData(String judul) {
+    public static Event getData(String path) {
         conn.connect();
 
-        String query = "SELECT * FROM events WHERE judul = ?";
+        String query = "SELECT * FROM events WHERE imagePath = ?";
         try {
             PreparedStatement statement = conn.con.prepareStatement(query);
-            statement.setString(1, judul); 
+            statement.setString(1, path);
             ResultSet rs = statement.executeQuery();
-            query = "";
-            Event event = new EventConcert(
-                rs.getInt("event_id"), 
-                judul, 
-                rs.getString("description"), 
-                rs.getString("imagePath"), 
-                rs.getDouble("harga"), 
-                rs.getInt("capacity"), 
-                rs.getDate("Date"),
-                rs.getString("Genre"), 
-                rs.getString("Singer")
-            );
+            Event event;
+            if (rs.next()) { // Pastikan ada data yang dikembalikan
+                if (rs.getString("kategori").equalsIgnoreCase(EventCat.MUSIC.name())) {
+                    event = new EventConcert(
+                            rs.getInt("event_id"),
+                            rs.getString("Judul"),
+                            rs.getString("description"),
+                            rs.getString("imagePath"),
+                            rs.getDouble("harga"),
+                            rs.getInt("capacity"),
+                            rs.getDate("Date"),
+                            rs.getString("Genre"),
+                            rs.getString("Singer"));
+                } else if (rs.getString("kategori").equalsIgnoreCase(EventCat.EDUCATION.name())) {
+                    event = new EventEducation(
+                            rs.getInt("event_id"),
+                            rs.getString("Judul"),
+                            rs.getString("description"),
+                            rs.getString("imagePath"),
+                            rs.getDouble("harga"),
+                            rs.getInt("capacity"),
+                            rs.getDate("Date"),
+                            rs.getString("Speaker"));
+                } else { // sport
+                    event = new EventSport(
+                            rs.getInt("event_id"),
+                            rs.getString("Judul"),
+                            rs.getString("description"),
+                            rs.getString("imagePath"),
+                            rs.getDouble("harga"),
+                            rs.getInt("capacity"),
+                            rs.getDate("Date"),
+                            rs.getString("SportType"));
+                }
+                return event;
+            } else {
+                System.err.println("No event found with the given image path: " + path);
+                return null;
+            }
 
-            return event;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
